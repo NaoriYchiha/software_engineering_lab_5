5fb8c754880b5d93eb6901cdd02829654403a584
NaoriYchiha <lolollb770@gmail.com>
2022-11-28 19:31:04 +0200

    Init commit

---
+++ 3.5/Source.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include <cmath>
+#include<fstream>
+
+using namespace std;
+
+int Only();
+int Hope();
+double UR(int x,int n);
+// Prototype //
+
+int main()
+{
+   
+    setlocale(LC_ALL, "Rus");
+    int n = Only();
+    int x =  Hope();
+    double VID = UR(x, n);
+    
+}
+
+
+int Only()// input n //
+{
+    int n;
+    cout << "¬ведите n: ";
+    cin >> n;
+    return n;
+
+}
+
+int Hope()// input x //
+{
+    int x;
+	cout << "¬ведите х: ";
+	cin >> x;
+	return x;
+}
+
+
+double UR(int x, int n) 
+{
+    setlocale(LC_ALL, "Rus");
+    double i, y, j, p = 0;
+
+    if (x > 0)// Calculation for x greater than nought //
+    {
+     
+        y = 1;
+        for (i = 1; i <= n; i++)
+        {
+            y *= ((1 / x) - (1 / i));
+        }
+        std::cout << "ќбласть значений:" << y;
+    }
+    else// Calculation for the rest x  //
+    {
+        y = 0;
+        for (i = 0; i <= n - 1; i++)
+        {
+            for (j = 0; j <= n - 1; j++)
+            {
+                p += 1 / (x - i + x * j);
+            }
+            y = y + p;
+            
+        }
+        std::cout << "ќбласть значений:" << y;
+    }
+    return y;
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "H:\программирование\c++\3.5\3.5\Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(UR_get3and2_0_5returned)//Test for positive value //
+		{
+			int x = 3;
+			int n = 2;
+			double expected = 0.5;
+
+			double actual = UR(x, n);
+
+			Assert::AreEqual(expected, actual);
+
+		}
+	public:
+
+		TEST_METHOD(UR_getnegative2and1_negative0_5returned)// Test for negative value //
+		{
+			int x = -2;
+			int n = 1;
+			double expected = -0.5;
+
+			double actual = UR(x, n);
+
+			Assert::AreEqual(expected, actual);	
+
+		}
+	}
+	;
+}
